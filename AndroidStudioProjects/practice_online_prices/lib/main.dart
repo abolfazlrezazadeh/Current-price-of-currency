a217ff98239a504077eaeb507f3a0c91c7dd07e6
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2025-01-08 12:36:40 +0330

    adding list view

--- AndroidStudioProjects/practice_online_prices/lib/main.dart
+++ AndroidStudioProjects/practice_online_prices/lib/main.dart
@@ -14,9 +14,10 @@ class MyApp extends StatelessWidget {
             textTheme: const TextTheme(
           titleLarge: TextStyle(fontSize: 17, fontWeight: FontWeight.w900),
           bodyLarge: TextStyle(fontSize: 17, fontWeight: FontWeight.w900),
-          bodyMedium: TextStyle(fontSize: 13, fontWeight: FontWeight.w700),
+          bodySmall: TextStyle(fontSize: 17, fontWeight: FontWeight.w700),
+          bodyMedium: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
           titleSmall: TextStyle(
-              color: Colors.white, fontSize: 12, fontWeight: FontWeight.w400),
+              color: Colors.white, fontSize: 14, fontWeight: FontWeight.w600),
         )),
         debugShowCheckedModeBanner: false,
         home: Home());
@@ -66,17 +67,19 @@ class Home extends StatelessWidget {
               ],
             ),
             Padding(padding: EdgeInsets.fromLTRB(0, 8, 0, 0)),
+
             Text(
-              "نرخ ارز ها در معاملات روزانه رایج است . معاملات نقدی معاملاتی  هستند که خریدار و فروشنده به محض انجام معامله ارز و ریال را باهم تبادل میکنند",
+              "نرخ ارز ها در معاملات روزانه رایج است .\n معاملات نقدی معاملاتی  هستند که خریدار و فروشنده به محض انجام معامله ارز و ریال را باهم تبادل میکنند.",
               style: Theme.of(context).textTheme.bodyMedium,
               textDirection: TextDirection.rtl,
             ),
+
             // body heead container
             Padding(
               padding: EdgeInsets.fromLTRB(25, 24, 0, 0),
               child: Container(
                 height: 35,
-                width: 350,
+                width: double.infinity,
                 decoration: BoxDecoration(
                   color: Color.fromARGB(255, 130, 130, 130),
                   borderRadius: BorderRadius.circular(100),
@@ -92,7 +95,46 @@ class Home extends StatelessWidget {
                   ],
                 ),
               ),
-            )
+            ),
+            // list
+            Container(
+                width: double.infinity,
+                // MediaQuery.of(context).size.height ==> height of screen
+                // 0.5 ==> 50% of screen
+                height: MediaQuery.of(context).size.height *0.5 ,
+                child: Directionality(
+                  textDirection: TextDirection.rtl,
+                  child: ListView.builder(
+                    physics: BouncingScrollPhysics(),
+                    itemCount: 10,
+                      itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.fromLTRB(25, 10, 0, 0),
+                      child: Container(
+                        width: double.infinity,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          boxShadow: <BoxShadow>[
+                            BoxShadow(blurRadius: 1.0, color: Colors.grey)
+                          ],
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(1000) 
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            //name of money
+                            Text("دلار",style: Theme.of(context).textTheme.bodySmall,),
+                            // price
+                            Text("85000",style: Theme.of(context).textTheme.bodySmall,),
+                            // changes
+                            Text("+3",style: Theme.of(context).textTheme.bodySmall,),
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
+                ))
           ],
         ),
       ),
